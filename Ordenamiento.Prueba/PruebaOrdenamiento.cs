6c2aa85474a3c49c12d92e875b2327abaeee41cb
Fernanda Morales <fmorales3@ucol.mx>
2019-03-25 22:11:08 -0600

    Se agrego el proyecto de pruebas

---
+++ Ordenamiento.Prueba/PruebaOrdenamiento.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Ordenamiento.Prueba
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
